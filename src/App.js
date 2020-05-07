07a01de294af11525ba5666aaeed6eb3af483bb8
wonjinsong <msmk22239@gmail.com>
2020-05-07 15:25:13 -0400

    useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './App.css';
 
@@ -13,25 +13,46 @@ function App() {
 }
 
 function FuncComp(props){
+  var numberState = useState(props.initNumber); //useState returns an array.
+  var number = numberState[0]; //state value
+  var setNumber = numberState[1];  //function which can change the state.
+
+  //var dateState = useState((new Date()).toString()); 
+  //var _date = dateState[0]; 
+  //var setDate = dateState[1];  
+  var [_date, setDate] = useState((new Date()).toString()); 
+  
   return (
     <div className="container">
       <h2>function style component</h2>
-      <p>Number : {props.initNumber}</p>
+      <p>Number : {number}</p>
+      <p>Date : {_date}</p>
+      <input type="button" value="random" onClick={
+          ()=>setNumber(Math.random())}>
+      </input><br/>
+      <input type="button" value="date" onClick={
+          ()=>setDate((new Date()).toString())}>
+      </input>
     </div>
   );
 }
 
 class ClassComp extends React.Component{
   state = {
-    number : this.props.initNumber
+    number : this.props.initNumber,
+    date : (new Date()).toString()
   }
   render(){
     return(
       <div className="container">
         <h2>class style component</h2>
         <p>Number : {this.state.number}</p>
+        <p>Date : {this.state.date}</p>
         <input type="button" value="random" onClick={
           ()=>this.setState({number:Math.random()})}>
+        </input><br/>
+        <input type="button" value="date" onClick={
+          ()=>this.setState({date:(new Date()).toString()})}>
         </input>
       </div>
     );
